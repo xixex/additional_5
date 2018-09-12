f55c4310e108d4621e6bad95fda26aa9c3e4191c
xixex <mikhailsontsav@gmail.com>
2018-09-12 16:38:28 +0300

    yes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var str2 = '';
+    var waitingBracket = -1;
+
+    for (let i = 0; i < str.length; i++) {
+        bracket = str[i];
+        findedArr = find(bracketsConfig, bracket);
+        if (str2 !== '') {
+            waitingBracket = find(bracketsConfig, str2[str2.length - 1])[2]
+        } else {
+            waitingBracket = 0;
+        }
+        if (findedArr[1] === 'opened' && bracket !== waitingBracket) {
+            str2 += bracket;
+        }
+        else if (bracket === waitingBracket || findedArr[2] === waitingBracket) {
+            str2 = str2.slice(0, -1);
+        }
+        else {
+            return false;
+        }
+    }
+
+    function find(arr, value) {
+        for (var i = 0; i < arr.length; i++)
+            if (arr[i][0] === value)
+                return [i, 'opened', arr[i][1]];
+            else if (arr[i][1] === value)
+                return [i, 'closed'];
+    }
+
+    console.log(str2 === '');
+    return str2 === '';
+};
+    
